7c56dfd9b5204a32245070cc3690603ff52ebd85
Harinder Dulai <harinder.dulai@godonate.digital>
2024-01-31 18:11:43 +0000

    router.push , router.replace, router.go(1 || -1)

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,7 +1,22 @@
 <script setup> 
+import { useRouter } from 'vue-router';
 const props = defineProps(["event"]); 
+const router = useRouter()
+const register = () => {
+    // if registraction API call is successfull
+    // push back to the event details
+    router.push({
+        name: 'EventDetails',
+    })
+
+    // Allows you to redirect without History, no going back to previous page
+    // router.replace({
+    //     name: 'EventDetails',
+    // })
+}
 </script>
 
 <template> 
     <p>Register for the event here</p> 
+    <button @click="register">Register Me!</button>
 </template>
